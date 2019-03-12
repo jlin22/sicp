12edaf76f98630ca3c259837d19ac6cb440f69bf
John Lin <jlin1674@gmail.com>
2019-03-12 15:32:44 -0400

    pairs as 2^a3^b

--- 2.1/pairs.scm
+++ 2.1/pairs.scm
@@ -6,3 +6,27 @@
 
 (define (cdr z)
   (z (lambda (p q) q)))
+
+(define (cons x y)
+  (define (dispatch m)
+	(cond ((= m 0) x)
+		  ((= m 1) y)
+		  (else ())))
+  dispatch)
+
+(define (car z) (z 0))
+(define (cdr z) (z 1))
+
+(define (cons x y)
+  (* (expt 2 x) (expt 3 y)))
+
+(define (car z)
+  (if (= 0 (remainder z 2))
+	(+ 1 (car (/ z 2)))
+	0))
+
+(define (cdr z)
+  (if (= 0 (remainder z 3))
+	(+ 1 (car (/ z 3)))
+	0))
+

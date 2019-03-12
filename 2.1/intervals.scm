6fc036916c74fbec076c6b2c1218de9c84014866
John Lin <jlin1674@gmail.com>
2019-03-12 16:01:37 -0400

    subtracting intervals complete

--- 2.1/intervals.scm
+++ 2.1/intervals.scm
@@ -20,3 +20,8 @@
 
 (define (upper-bound x) (cdr x))
 (define (lower-bound x) (car x))
+
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+				 (- (upper-bound x) (lower-bound y))))
+				

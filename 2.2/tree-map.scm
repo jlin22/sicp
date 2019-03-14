fd51b53df1d14c29246fb9d09a204a3f9e38e4ee
John Lin <jlin1674@gmail.com>
2019-03-14 17:28:36 -0400

    finished tree-square

---
+++ 2.2/tree-map.scm
@@ -0,0 +1,7 @@
+(define (tree-map proc tree)
+  (cond ((null? tree) ())
+		((not (pair? tree)) (proc tree))
+		(else (cons (tree-map proc (car tree))
+				    (tree-map proc (cdr tree))))))
+
+(define (square-tree tree) (tree-map square tree))

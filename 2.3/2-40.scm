969a16238614a39506ac96ea3a8b5ef0d5669487
John Lin <jlin1674@gmail.com>
2019-03-30 20:08:18 -0400

    exercise 2-40

---
+++ 2.3/2-40.scm
@@ -0,0 +1,18 @@
+(define (unique-pairs n)
+  (accumulate append
+	      ()
+	      (map (lambda (i)
+		     (map (lambda (j)
+			    (list i j))
+			  (enumerate-interval 1 (-i 1))))
+		   (enumerate-interval 1 n))))
+
+(define (enumerate-interval s e)
+  (if (> s e)
+      ()
+      (cons s (enumerate-interval (+ s 1) e))))
+
+(define (accumulate proc init seq)
+  (if (null? seq)
+      init
+      (proc (car seq) (accumulate proc init (cdr seq)))))

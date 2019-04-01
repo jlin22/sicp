d1a2404df8bf529cc95db31ddb4b4045f5e844c8
John Lin <jlin1674@gmail.com>
2019-04-01 17:16:15 -0400

    need to think about 2-42 more

--- 2.3/2-42.scm
+++ 2.3/2-42.scm
@@ -12,12 +12,3 @@
 	  (queen-cols (- k 1))))))
   (queens-cols board-size))
 
-(define (adjoin-position new-row k rest-of-queens)
-  (append rest-of-queens (empty-row (- k 1))))
-  
-(define (empty-row size)
-  (if (= size 0)
-      ()
-      (cons 0 (empty-row (- size 1)))))
-
-(define empty-board (list ()))

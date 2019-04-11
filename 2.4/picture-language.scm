cbb342ee83a998f0dba10ca866937b76325747b3
John Lin <jlin1674@gmail.com>
2019-04-10 20:50:46 -0400

    finished flip-horiz, rotate180, rotate270

--- 2.4/picture-language.scm
+++ 2.4/picture-language.scm
@@ -175,5 +175,22 @@
 	    (paint-left frame)
 	    (paint-right frame))))))
 
+(define (flip-horiz painter)
+  (transform-painter painter
+		     (make-vect 1.0 0.0)
+		     (make-vect 0.0 0.0)
+		     (make-vect 1.0 1.0)))
 
+(define (rotate180 painter)
+  (transform-painter painter
+		     (make-vect 1.0 1.0)
+		     (make-vect 1.0 0.0)
+		     (make-vect 0.0 1.0)))
+
+(define (rotate270 painter)
+  (transform-painter painter
+		     (make-vect 0.0 1.0)
+		     (make-vect 1.0 1.0)
+		     (make-vect 0.0 0.0)))
+  
        

be59dbb0bd55fc144a46cac0e216ee31e7dcba69
John Lin <jlin1674@gmail.com>
2019-04-10 20:07:13 -0400

    up to frames in my picture language

--- 2.4/picture-language.scm
+++ 2.4/picture-language.scm
@@ -1,5 +1,7 @@
 (define wave2 (beside wave (flip-vert wave)))
 
+;(define wave4 (below wave2 wave2))
+	       
 (define wave4 (flipped-pairs wave))
 
 (define (right-split painter n)
@@ -44,4 +46,26 @@
   (let ((combine4 (square-of-four identity flip-vert
 				  identity flip-vert)))
     (combine4 painter)))
-	      
+
+(define (square-limit painter n)
+  (let ((combine4 (square-of-four flip-horiz identity
+				  rotate180 flip-vert)))
+    (combine4 (corner-split painter n))))
+
+(define right-split (split beside below))
+(define up-split (split below beside))
+
+(define (split trans1 trans2)
+  (lambda (painter)
+    (trans1 painter (trans2 painter painter))))
+
+(define (frame-coord-map frame)
+  (lambda (v)
+    (add-vect
+     (origin-frame frame)
+     (add-vect (scale-vect (xcor-vect v)
+			   (edge1-frame frame))
+	       (scale-vect (ycor-vect v)
+			   (edge2-frame frame))))))
+
+
